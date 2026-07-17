ac53d49ac6ceb2d90cd786e55da3837666ccfb0e
bePramudya <bepramudyaa@gmail.com>
2026-07-17 13:36:37 +0700

    feat: add command 'explore'

--- main.go
+++ main.go
@@ -7,6 +7,7 @@ import (
 	"io"
 	"net/http"
 	"os"
+	"strings"
 )
 
 func main() {
@@ -17,20 +18,26 @@ func main() {
 
 		scanner.Scan()
 
-		words := scanner.Text()
-
-		if len(words) == 0 {
+		input := scanner.Text()
+		if len(input) == 0 {
 			continue
 		}
 
-		command, exist := registry()[words]
+		inputs := strings.Fields(input)
 
+		firstInput := inputs[0]
+		secondInput := ""
+		if len(inputs) > 1 {
+			secondInput = inputs[1:][0]
+		}
+
+		command, exist := commands()[firstInput]
 		if !exist {
 			fmt.Print("Unknown command \n")
 			continue
 		}
 
-		err := command.callback(&config)
+		err := command.callback(&config, secondInput)
 		if err != nil {
 			fmt.Println(err)
 		}
@@ -41,17 +48,17 @@ func main() {
 	}
 }
 
-func commandExit(c *Config) error {
+func commandExit(c *Config, val string) error {
 	fmt.Print("Closing the Pokedex... Goodbye!\n")
 	os.Exit(0)
 	return nil
 }
 
-func commandHelp(c *Config) error {
+func commandHelp(c *Config, val string) error {
 	fmt.Print("Welcome to the Pokedex!\n")
 	fmt.Print("Usage:\n\n")
 
-	for _, reg := range registry() {
+	for _, reg := range commands() {
 		fmt.Print(reg.name, ": ", reg.description, "\n")
 	}
 
@@ -71,7 +78,7 @@ func helperMapsCommands(c *Config, target string) error {
 		return err
 	}
 
-	var areas LocationAreas
+	var areas RespSearchLocation
 	err = json.Unmarshal(body, &areas)
 	if err != nil {
 		fmt.Println(err)
@@ -87,7 +94,7 @@ func helperMapsCommands(c *Config, target string) error {
 	return nil
 }
 
-func commandMap(c *Config) error {
+func commandMap(c *Config, val string) error {
 	if c.Next == "" {
 		return fmt.Errorf("map search reached limit")
 	}
@@ -100,7 +107,7 @@ func commandMap(c *Config) error {
 	return nil
 }
 
-func commandMapBack(c *Config) error {
+func commandMapBack(c *Config, val string) error {
 	if c.Previous == "" {
 		return fmt.Errorf("There's no previous map search")
 	}
@@ -113,13 +120,49 @@ func commandMapBack(c *Config) error {
 	return nil
 }
 
+func commandExplore(c *Config, val string) error {
+	if val == "" {
+		return fmt.Errorf("No location specified, command: explore <location>")
+	}
+
+	url := "https://pokeapi.co/api/v2/location-area/" + val
+
+	res, err := http.Get(url)
+	if err != nil {
+		return err
+	}
+	defer res.Body.Close()
+
+	body, err := io.ReadAll(res.Body)
+	if res.StatusCode == 404 {
+		return fmt.Errorf("location not found")
+	} else if res.StatusCode > 299 {
+		return fmt.Errorf("API response failed with status code: %d and \nbody: %s\n", res.StatusCode, body)
+	}
+
+	var locationDetail = RespLocationDetail{}
+	err = json.Unmarshal(body, &locationDetail)
+	if err != nil {
+		return err
+	}
+
+	fmt.Print("Exploring ", locationDetail.Name, "... \n")
+	fmt.Print("Found Pokemon: \n")
+
+	for _, encounter := range locationDetail.PokemonEncounters {
+		fmt.Print(encounter.Pokemon.Name, "\n")
+	}
+
+	return nil
+}
+
 type cliCommand struct {
 	name        string
 	description string
-	callback    func(c *Config) error
+	callback    func(c *Config, val string) error
 }
 
-func registry() map[string]cliCommand {
+func commands() map[string]cliCommand {
 	return map[string]cliCommand{
 		"exit": {
 			name:        "exit",
@@ -141,6 +184,11 @@ func registry() map[string]cliCommand {
 			description: "Display previous map search",
 			callback:    commandMapBack,
 		},
+		"explore": {
+			name:        "explore",
+			description: "explore a region",
+			callback:    commandExplore,
+		},
 	}
 }
 
@@ -154,7 +202,7 @@ var config = Config{
 	Previous: "",
 }
 
-type LocationAreas struct {
+type RespSearchLocation struct {
 	Count    int    `json:"count"`
 	Next     string `json:"next"`
 	Previous string `json:"previous"`
@@ -162,4 +210,57 @@ type LocationAreas struct {
 		Name string `json:"name"`
 		URL  string `json:"url"`
 	} `json:"results"`
+}
+
+type RespLocationDetail struct {
+	EncounterMethodRates []struct {
+		EncounterMethod struct {
+			Name string `json:"name"`
+			URL  string `json:"url"`
+		} `json:"encounter_method"`
+		VersionDetails []struct {
+			Rate    int `json:"rate"`
+			Version struct {
+				Name string `json:"name"`
+				URL  string `json:"url"`
+			} `json:"version"`
+		} `json:"version_details"`
+	} `json:"encounter_method_rates"`
+	GameIndex int `json:"game_index"`
+	ID        int `json:"id"`
+	Location  struct {
+		Name string `json:"name"`
+		URL  string `json:"url"`
+	} `json:"location"`
+	Name  string `json:"name"`
+	Names []struct {
+		Language struct {
+			Name string `json:"name"`
+			URL  string `json:"url"`
+		} `json:"language"`
+		Name string `json:"name"`
+	} `json:"names"`
+	PokemonEncounters []struct {
+		Pokemon struct {
+			Name string `json:"name"`
+			URL  string `json:"url"`
+		} `json:"pokemon"`
+		VersionDetails []struct {
+			EncounterDetails []struct {
+				Chance          int           `json:"chance"`
+				ConditionValues []interface{} `json:"condition_values"`
+				MaxLevel        int           `json:"max_level"`
+				Method          struct {
+					Name string `json:"name"`
+					URL  string `json:"url"`
+				} `json:"method"`
+				MinLevel int `json:"min_level"`
+			} `json:"encounter_details"`
+			MaxChance int `json:"max_chance"`
+			Version   struct {
+				Name string `json:"name"`
+				URL  string `json:"url"`
+			} `json:"version"`
+		} `json:"version_details"`
+	} `json:"pokemon_encounters"`
 }
